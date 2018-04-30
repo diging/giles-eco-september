2b047592a91071e7ff3420c8168390f99a78769a
abhikrish503501 <abhijithkrishnan503501@gmail.com>
2018-04-30 14:46:19 -0700

    Story/geco 78 (#11)
    * [GECO-67] Pagination Style has been modified in September
    
    * [GECO-79] Modified the existing pagination with the Digg-style Pagination in September
    
    * [GECO-79] Modified the pagination with the simplepagination.js plugin
    
    * [GECO-78] Made changes for including checkboxes for filtering
    
    * [GECO-78] Made changes for filtering using jQuery DataTables
    
    * [GECO-78] Filtering Message type with checkboxes is implemented
    
    * [GECO-78] Included the DataTable with client side filtering
    
    * [GECO-78] Formatted properly and deleted all the unwanted comments and methods
    
    * [GECO-78] Made some changes for server side Datatable processing
    
    * [GECO-78] Made some changes for server side Datatable processing with ajax call
    
    * [GECO-78] Made some more changes for server side filtering except controller part
    
    * [GECO-78] Made some more changes for server side filtering along with setting permissionfor new links
    
    * [GECO-78] Made some more changes for filtering and pagination by creating new classes for datatable attributes
    
    * Integrated DataTable plugin instead of normal table through server side processing
    
    * Datatable attributes added and started filtering method
    
    * [GECO-78] Set proper offset for retrieving data.
    
    * [GECO-78] Render filtering values from checkbox to Datatables.
    
    * [GECO-78] Added filtering with external multiple checkboxes in the datatable.
    
    * [GECO-78] Formatted all the changed files and made some changes in the ajax call
    
    * [GECO-78] Removed the changes made in project file
    
    * [GECO-78] Fixed formatting
    
    * Removed print statements and renamed methods in proper convention
    
    * [GECO-78] Removed string parsing from db connector and added in to service, also addressed all the review comments
    
    * [GECO-78] Addressed all review comments
    
    * [GECO-78] Addressed all the review comments
    
    * [GECO-78] fixing pageSize, some display issues and layout
    
    * [GECO-78] delete .DS_Store
    
    * removed .DS_Store file
    
    * [GECO-78] shortened code; fixed method naming;

---
+++ september/src/main/java/edu/asu/diging/gilesecosystem/september/web/DataTableData.java
@@ -0,0 +1,64 @@
+package edu.asu.diging.gilesecosystem.september.web;
+
+import java.util.List;
+
+import edu.asu.diging.gilesecosystem.september.core.model.IMessage;
+
+/**
+ * DataTableData class is for setting and retrieving various 
+ * attributes required for JS DataTable plug-in which can be used to
+ * display the kind of messages and it can be filtered by the message type
+ * @author Abhijith Krishnan Radhakrishna Kurup
+ */
+
+public class DataTableData {
+
+    private int draw;
+    private int recordsTotal;
+    private int recordsFiltered;
+    private List<IMessage> data;
+    private int pageSize;
+
+    public int getDraw() {
+        return draw;
+    }
+
+    public int getRecordsTotal() {
+        return recordsTotal;
+    }
+
+    public int getRecordsFiltered() {
+        return recordsFiltered;
+    }
+
+    public List<IMessage> getData() {
+        return data;
+    }
+
+    public void setDraw(int draw) {
+        this.draw = draw;
+    }
+
+    public void setRecordsTotal(int recordsTotal) {
+        this.recordsTotal = recordsTotal;
+
+    }
+
+    public void setRecordsFiltered(int recordsFiltered) {
+        this.recordsFiltered = recordsFiltered;
+
+    }
+
+    public void setData(List<IMessage> data) {
+        this.data = data;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+}
